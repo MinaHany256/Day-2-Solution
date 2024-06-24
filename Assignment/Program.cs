bd002fd3678bb5185c690b558acc512b481137c1
MinaHany256 <minahanyfoad@gmail.com>
2024-06-24 22:56:22 +0300

    Part 09 ValueType Vs ReferenceType [Code]

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -26,6 +26,38 @@
 
             //Console.WriteLine(id); 
             #endregion
+
+            #region Value Type
+            //int X;
+            ////CLR will Allocate 4 unintialized Bytes at the STACK
+            ////int : C# Keyword
+
+            //X = 5;
+            //Int32 Y = 10;
+            ////CLR will Allocate 4 unintialized Bytes at the STACK
+            ////Int32 : BCL
+
+            //Y = X;  //5
+            //X++;  //6
+            //Console.WriteLine(Y); 
+            #endregion
+
+            #region Reference Type
+            //Point P1;
+            ////Declare For Reference of type "Point" refering to NULL
+            ////This Reference 'P1' Can Refer To an Object From Type "Point" OR another type "Inherting From Point"
+            ////CLR will Allocate 4 bytes for the reference at the STACK
+            ////CLR will Allocate 0 bytes At HEAP
+
+            //P1 = new Point();
+
+            //Point P2 = new Point();
+
+            //P2 = P1;
+            //P2.x = 6;
+            //Console.WriteLine(P1.x);
+            #endregion
+
         }
     }
 }
