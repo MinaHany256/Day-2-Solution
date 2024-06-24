1d873e5998b32d522a3e44412fcc855f709a5b1b
MinaHany256 <minahanyfoad@gmail.com>
2024-06-24 23:20:10 +0300

    Session Task

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Please Enter Your Name : ");
+            String Name = Console.ReadLine() ?? "NA";
+
+            Console.Write("Please Enter Your Age : ");
+            int.TryParse(Console.ReadLine(), out int Age);
+
+            Console.Write("Please Enter Your Salary : ");
+            int.TryParse(Console.ReadLine(), out int Salary);
+
+            Console.Clear();
+
+            Console.WriteLine($"Your Name is { Name }");
+            Console.WriteLine($"Your Age is { Age }");
+            Console.WriteLine($"Your Salary is { Salary }");
+
         }
     }
 }
