29d6904ad323dd0c42714b6d953e96ae62bfb0ac
MinaHany256 <minahanyfoad@gmail.com>
2024-06-24 22:58:57 +0300

    Part 10 Object

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -58,6 +58,29 @@
             //Console.WriteLine(P1.x);
             #endregion
 
+            #region Object
+            //Point P1 = new Point();
+            //Point P2 = new Point();
+
+            //Console.WriteLine( P1.ToString() ); //Demo.Point   Namespace of Point + class name of p1
+
+            //Console.WriteLine( P1.GetHashCode() );
+            //Console.WriteLine( P2.GetHashCode() );
+            //Console.WriteLine(P1.Equals(P2));   //False  
+
+            //object O1;
+            //Declare for reference of type "Object" , refering to NULL
+            //4 bytes been allocated at STACK for the reference 'O1'
+            //0 bytes have been allocated at HEAP
+
+            //O1 = new object();
+            //O1 = "Ahmed";
+            //O1 = 5;  // Boxing
+
+
+
+            #endregion
+
         }
     }
 }
