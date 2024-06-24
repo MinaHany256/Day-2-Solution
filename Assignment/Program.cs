6112cd95e5bdd6c38e319a41db0fb35bebf2a568
MinaHany256 <minahanyfoad@gmail.com>
2024-06-24 21:29:10 +0300

    Question 2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -13,6 +13,15 @@
 
             #endregion
 
+            #region 2-Write C# program that Convert a string to an integer, but the string contains non-numeric characters. And mention what will happen 
+
+            //String Number1 = "Ahmed";
+            //int Number2 = Convert.ToInt32(Number1);
+            //Console.WriteLine(Number2);
+            //// An Exception will be thrown 
+
+            #endregion
+
 
         }
     }
